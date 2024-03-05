de23390f23eabf67481409e31c45743806f1bdd3
Jang-Jaewon <jewon119@gmail.com>
2024-03-05 17:10:10 +0900

    fix: env 방식에서 python 파일에서 로드하는 방식으로 변경

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ sys.path.insert(0, str(app_dir))
 from contextlib import asynccontextmanager
 
 import uvicorn
-from core.setting import ALLOWED_ORIGINS, APP_ENV, DEBUG
+from core.config import APP_ENV, DEBUG, ALLOWED_ORIGINS
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -34,18 +34,15 @@ app.add_middleware(
 def api_health_check():
     return {
         "api_health_check": "api-server is Ok",
-        "debug-mode": DEBUG,
+        "APP_ENV": APP_ENV,
+        "DEBUG": DEBUG,
+        "ALLOWED_ORIGINS": ALLOWED_ORIGINS
     }
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
-
-
-@app.get("/env_test")
-def read_env():
-    return {"APP_ENV": APP_ENV, "DEBUG": DEBUG, "ALLOWED_ORIGINS": ALLOWED_ORIGINS}
 
 
 if __name__ == "__main__":
