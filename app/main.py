82447dad87950669d6b3f46c902ec05788710e65
Jang-Jaewon <jewon119@gmail.com>
2024-03-05 15:02:29 +0900

    fix: dot-env 방식으로 수정

--- app/main.py
+++ app/main.py
@@ -1,32 +1,24 @@
-import sys
-from pathlib import Path
-
-root_path = Path(__file__).resolve().parent.parent
-sys.path.append(str(root_path))
-
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from core.setting import settings
+from app.core.setting import APP_ENV, DEBUG, ALLOWED_ORIGINS
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print(f"INFO:     Hello, Run in the pyinstaller environment 👋")
-    # print(f"INFO:     Hello, Run in the {settings.APP_ENV} environment 👋")
+    print(f"INFO:     Hello, Run in the {APP_ENV} environment 👋")
     yield
-    print(f"INFO:     Bye, Shut down in the pyinstaller environment 👋")
-    # print(f"INFO:     Bye, Shut down in the {settings.APP_ENV} environment 👋")
+    print(f"INFO:     Bye, Shut down in the {APP_ENV} environment 👋")
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://localhost:3000"],
+    allow_origins=ALLOWED_ORIGINS,
     # allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
@@ -38,7 +30,7 @@ app.add_middleware(
 def api_health_check():
     return {
         "api_health_check": "api-server is Ok",
-        "debug-mode": "Pyinstaller",
+        "debug-mode": DEBUG,
         # "debug-mode": settings.DEBUG,
     }
 
@@ -48,5 +40,14 @@ def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
 
+@app.get("/env_test")
+def read_env():
+    return {
+        "APP_ENV": APP_ENV,
+        "DEBUG": DEBUG,
+        "ALLOWED_ORIGINS": ALLOWED_ORIGINS
+    }
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
