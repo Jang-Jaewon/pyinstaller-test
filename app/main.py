2336161edc069f54875114fc612e087a06d124aa
Jang-Jaewon <jewon119@gmail.com>
2024-03-05 12:14:54 +0900

    fix: uvicorn 실행 부분 수정

--- app/main.py
+++ app/main.py
@@ -10,21 +10,24 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.core.setting import settings
+# from core.setting import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print(f"INFO:     Hello, Run in the {settings.APP_ENV} environment 👋")
+    print(f"INFO:     Hello, Run in the pyinstaller environment 👋")
+    # print(f"INFO:     Hello, Run in the {settings.APP_ENV} environment 👋")
     yield
-    print(f"INFO:     Bye, Shut down in the {settings.APP_ENV} environment 👋")
+    print(f"INFO:     Bye, Shut down in the pyinstaller environment 👋")
+    # print(f"INFO:     Bye, Shut down in the {settings.APP_ENV} environment 👋")
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=["http://localhost:8000", "http://localhost:3000"],
+    # allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -35,7 +38,8 @@ app.add_middleware(
 def api_health_check():
     return {
         "api_health_check": "api-server is Ok",
-        "debug-mode": settings.DEBUG,
+        "debug-mode": "Pyinstaller",
+        # "debug-mode": settings.DEBUG,
     }
 
 
@@ -45,4 +49,4 @@ def read_item(item_id: int, q: str | None = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
