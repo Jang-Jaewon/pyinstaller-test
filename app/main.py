b8d3026e7d1a63a50a171a925b668e3fcca57fff
Jang-Jaewon <jewon119@gmail.com>
2024-03-25 12:11:30 +0900

    fix: main.py host change

--- app/main.py
+++ app/main.py
@@ -40,4 +40,5 @@ def api_health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="localhost", port=8000)
